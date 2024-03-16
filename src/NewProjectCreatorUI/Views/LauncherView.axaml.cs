6c49682f4ec611924d206cca5a804e2c287da1a1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-16 21:57:22 +0000

    create Launcher view

---
+++ src/NewProjectCreatorUI/Views/LauncherView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NewProjectCreatorUI.Views;
+
+public partial class LauncherView : UserControl
+{
+    public LauncherView()
+    {
+        InitializeComponent();
+    }
+}
