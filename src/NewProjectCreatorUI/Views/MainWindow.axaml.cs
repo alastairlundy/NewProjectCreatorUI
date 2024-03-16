413c9389c62138670fd986673a261096d07b6531
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-16 21:57:03 +0000

    start working on Settings loading

--- src/NewProjectCreatorUI/Views/MainWindow.axaml.cs
+++ src/NewProjectCreatorUI/Views/MainWindow.axaml.cs
@@ -1,11 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using Avalonia.Controls;
+using SettingsKit;
+using SettingsKit.Providers;
 
 namespace NewProjectCreatorUI.Views;
 
 public partial class MainWindow : Window
 {
+    private SettingsManager<string> _settingsManager;
+
+    private SettingsFile<string> _launcherSettingsFile;
+    
     public MainWindow()
     {
         InitializeComponent();
+        
+        _settingsManager = new SettingsManager<string>();
+        var launcherSettingsFile = new SettingsFile<string>(Environment.CurrentDirectory + Path.DirectorySeparatorChar + "Resources" + Path.DirectorySeparatorChar + "Settings" + Path.DirectorySeparatorChar + "LauncherSettings.json", new JsonSettingsFileProvider<string>());
+        
+        LoadSettings();
+    }
+
+    private void LoadSettings()
+    {
+        this.Width = double.Parse(_launcherSettingsFile.Get("Width.Default"));
+        this.Height = double.Parse(_launcherSettingsFile.Get("Height.Default"));
     }
 }
