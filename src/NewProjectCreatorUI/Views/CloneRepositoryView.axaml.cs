18b7d28b2786c52f816f67bb445fe64271212000
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-22 14:46:16 +0000

    add code to handle button presses in Clone Repository View

--- src/NewProjectCreatorUI/Views/CloneRepositoryView.axaml.cs
+++ src/NewProjectCreatorUI/Views/CloneRepositoryView.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace NewProjectCreatorUI.Views;
@@ -9,5 +10,35 @@ public partial class CloneRepositoryView : UserControl
     public CloneRepositoryView()
     {
         InitializeComponent();
+    }
+
+    public void PathBrowseButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+       
+    }
+
+    private void CloneButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void BackButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+       
+    }
+
+    private void GitLabConnectButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+       
+    }
+
+    private void AzureDevOpsConnectButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+       
+    }
+
+    private void GitHubConnectButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+       
     }
 }
